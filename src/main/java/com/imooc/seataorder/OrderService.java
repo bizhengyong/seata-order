c9d8a34417686ccabc2687b29d9918d95b71c81d
bizhengyong <bizhengyong_xy@163.com>
2021-03-01 20:57:30 +0800

    11-2-RestTemplate和分布式事务(下)

--- src/main/java/com/imooc/seataorder/OrderService.java
+++ src/main/java/com/imooc/seataorder/OrderService.java
@@ -1,6 +1,8 @@
 package com.imooc.seataorder;
 
+import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,10 +14,14 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class OrderService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GlobalTransactional
     public Boolean create(Integer count){
         //调用product扣库存
         String url = "http://localhost:8086/deduct?productId=5001&count="+count;
-        Boolean result = new RestTemplate().getForObject(url, Boolean.class);
+        Boolean result = restTemplate.getForObject(url, Boolean.class);
         if (result != null && result) {
             //可能抛出异常
             if (5 == count) {
